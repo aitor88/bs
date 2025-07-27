a0f1094937194d3ec3305a9209e36d4cae27653e
aitor88 <nireapos@gmail.com>
2025-07-27 10:40:38 +0200

    Create audio.js

---
+++ js/audio.js
@@ -0,0 +1,24 @@
+function unlockAudio() {
+    if (audioUnlocked) return;
+    for (const key in sounds) {
+        sounds[key].play().catch(() => {});
+        sounds[key].pause();
+        sounds[key].currentTime = 0;
+    }
+    audioUnlocked = true;
+}
+
+function playSound(sound) {
+    if (!audioUnlocked) return;
+    sound.currentTime = 0;
+    sound.play().catch(e => {});
+}
+
+function stopAllMusic() {
+    sounds.menuMusic.pause();
+    sounds.gameMusic.pause();
+    sounds.defeatMusic.pause();
+    sounds.menuMusic.currentTime = 0;
+    sounds.gameMusic.currentTime = 0;
+    sounds.defeatMusic.currentTime = 0;
+}
